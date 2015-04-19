c0ffce95fa0c2ab8fe696490cda48f2d9e4c2841
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-19 16:59:28 +0300

    Your format2

--- grus-tournament-2015/Problem1/Program.cs
+++ grus-tournament-2015/Problem1/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Problem1
+public class Program
 {
-    public class Program
-    {
-        public static char[] _alfa = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-        public static void Main(string[] args)
-        {
-                string input = Console.ReadLine();
-                Console.WriteLine(DeCrypt(input, 1));
-        }
-        public static string DeCrypt(string input, int key)
-        {
-            string result = "";
-            for (int i = 0; i < input.Length; i++)
-            {
-                int number = GetNumber(input[i]);
-                int newNumber = (number + key)%26;
-                result = result + _alfa[newNumber];
-            }
-            return result;
-        }
+    public static char[] _alfa = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
 
-        public static int GetNumber(char c)
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(DeCrypt(input, 1));
+    }
+
+    public static string DeCrypt(string input, int key)
+    {
+        string result = "";
+        for (int i = 0; i < input.Length; i++)
         {
-            int number = 0;
-            for (int i = 0; i < _alfa.Length; i++)
-            {
-                if (c == _alfa[i])
-                {
-                    number = i;
-                }
-            }
-            return number;
+            int number = GetNumber(input[i]);
+            int newNumber = (number + key)%26;
+            result = result + _alfa[newNumber];
         }
+        return result;
+    }
+
+    public static int GetNumber(char c)
+    {
+        int number = 0;
+        for (int i = 0; i < _alfa.Length; i++)
+        {
+            if (c == _alfa[i])
+            {
+                number = i;
+            }
+        }
+        return number;
     }
 }
+
