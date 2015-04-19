f6babc1c17f260ad1da63e7a3ec4a7576e3179b3
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-19 18:03:39 +0300

    Problem5

---
+++ grus-tournament-2015/Problem5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        byte[] bStr = Encoding.GetEncoding("koi8r").GetBytes(str);
+        string str_new = Encoding.GetEncoding(1251).GetString(bStr);
+        Console.WriteLine(str_new);
+    }
+}
+
