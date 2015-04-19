c5123793b3b3f347a56fd17f717fefa8452011f1
Aliaksei Piarouski <perovskijab@gmail.com>
2015-04-19 16:08:24 +0300

    problem0, empty problem2

---
+++ grus-tournament-2015/Problem0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problem0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
