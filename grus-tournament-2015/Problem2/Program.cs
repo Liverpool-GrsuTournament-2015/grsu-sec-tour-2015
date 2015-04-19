6d183606bfb7c0f889b33b42545d73312eb13af5
Aliaksei Piarouski <perovskijab@gmail.com>
2015-04-19 16:43:59 +0300

    problem2 resolved

--- grus-tournament-2015/Problem2/Program.cs
+++ grus-tournament-2015/Problem2/Program.cs
@@ -10,7 +10,28 @@ namespace Problem2
     {
         static void Main(string[] args)
         {
+            var offset = 13;
+            var encrypted = Console.ReadLine();
 
+            Console.WriteLine(Decrypt(encrypted, offset));
+
+        }
+
+        private static string Decrypt(string encrypted, int offset)
+        {
+            const char a_char = 'a';
+            const char z_char = 'z';
+            int alfabetLength = z_char - a_char + 1;
+
+            var decrypted = new StringBuilder(encrypted.Length);
+
+            foreach (char encr_char in encrypted)
+            {
+                int decr_char = ((encr_char - a_char) - offset + alfabetLength)%alfabetLength;
+                decrypted.Append((char)(decr_char + a_char));
+            }
+
+            return decrypted.ToString();
         }
     }
 }
