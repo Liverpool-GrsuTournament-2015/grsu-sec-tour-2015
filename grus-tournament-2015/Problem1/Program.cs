aefc147f4c8f03a47c4b5c6d3464190687324939
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-19 16:35:26 +0300

    Task1

--- grus-tournament-2015/Problem1/Program.cs
+++ grus-tournament-2015/Problem1/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Problem1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static char[] _alfa = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static void Main(string[] args)
         {
+            int nTests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nTests; i++)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(DeCrypt(input, 1));
+            }
+        }
+        public static string DeCrypt(string input, int key)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number = GetNumber(input[i]);
+                int newNumber = (number + key)%26;
+                result = result + _alfa[newNumber];
+            }
+            return result;
+        }
+
+        public static int GetNumber(char c)
+        {
+            int number = 0;
+            for (int i = 0; i < _alfa.Length; i++)
+            {
+                if (c == _alfa[i])
+                {
+                    number = i;
+                }
+            }
+            return number;
         }
     }
 }
