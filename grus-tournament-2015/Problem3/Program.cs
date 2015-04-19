e5a3c445dba14f5832ddbd896baacc7b7f712be7
Левчук Антон Анатольевич <anton93lev@mail.ru>
2015-04-19 17:50:31 +0300

    Problem3

---
+++ grus-tournament-2015/Problem3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        byte[] arr = new byte[input.Length / 8];
+        for (int i = 0; i < input.Length; i += 8)
+        {
+            string ss = input.Substring(i, 8);
+            byte bv = Convert.ToByte(ss, 2);
+            arr[i/8] = bv;
+        }
+        string res = Encoding.UTF8.GetString(arr);
+        Console.WriteLine(res);
+    }
+}
+
